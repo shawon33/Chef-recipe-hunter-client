e7a6d460a1e7ab98ddb85718611eb8d1099e966e
shawon33 <iamcoming115@gmail.com>
2023-06-22 06:20:47 +0600

    authentication troggle is done n

--- src/AuthContext/AuthProvider.jsx
+++ src/AuthContext/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createContext } from "react";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
@@ -7,18 +7,34 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
 
+    const [user, setUser] = useState(null);
+
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     };
 
     const signIn = (email, password) => {
-        
         return signInWithEmailAndPassword(auth, email, password)
     };
+    const logOut = ()=>{
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            console.log("logged in user inside auth state observer", loggedUser);
+            setUser(loggedUser)
+        })
+        return()=>{
+         unsubscribe();
+        }
+    },[]);
 
     const authInfo = {
+        user,
         createUser,
-        signIn
+        signIn,
+        logOut
     }
 
     return (
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,18 @@
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FaGulp} from 'react-icons/fa';
+import { FaGulp, FaUserCircle} from 'react-icons/fa';
+import { useContext } from "react";
+import { AuthContext } from "../AuthContext/AuthProvider";
 
 
 const Header = () => {
+   const {user,logOut} =useContext(AuthContext);
+
+    const handleLogOut =()=>{
+        logOut()
+        .then()
+        .catch(error=> console.log(error))
+    }
    return (
       <Container className="mt-4">
          
@@ -24,10 +33,8 @@ const Header = () => {
                      <Nav.Link href="#pricing">Blogs</Nav.Link>
      
                   </Nav>
+                  
                   <Nav>
-                     <Link to='/login'><Button variant="secondary">Login</Button></Link>
-                  </Nav>
-                  {/* <Nav>
                      {
                         user && <FaUserCircle style={{ fontSize: '2rem' }}></FaUserCircle>
                      }
@@ -37,7 +44,7 @@ const Header = () => {
                            <Button onClick={handleLogOut} variant="secondary">LogOut</Button> :
                            <Link to='/login'><Button variant="secondary">Login</Button></Link>
                      }
-                  </Nav> */}
+                  </Nav>
                </Navbar.Collapse>
             </Container>
          </Navbar>
