3450aba7db6fe77c2b5f8962f66796fcae369835
shawon33 <iamcoming115@gmail.com>
2023-06-29 07:10:24 +0600

    google singin method is work properly

--- src/AuthContext/AuthProvider.jsx
+++ src/AuthContext/AuthProvider.jsx
@@ -8,6 +8,7 @@ const auth = getAuth(app);
 const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null);
+    console.log(user);
 
 
 
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,21 +1,21 @@
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { FaGulp, FaUserCircle} from 'react-icons/fa';
+import { FaGulp, FaUserCircle } from 'react-icons/fa';
 import { useContext } from "react";
 import { AuthContext } from "../AuthContext/AuthProvider";
 
 
 const Header = () => {
-   const {user,logOut} =useContext(AuthContext);
+   const { user, logOut } = useContext(AuthContext);
 
-    const handleLogOut =()=>{
-        logOut()
-        .then()
-        .catch(error=> console.log(error))
-    }
+   const handleLogOut = () => {
+      logOut()
+         .then()
+         .catch(error => console.log(error))
+   }
    return (
       <Container className="mt-4">
-         
+
          <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
                <h1 className="text-success"><FaGulp className="pb-2"></FaGulp>Coo<span className="text-warning">ked</span></h1>
@@ -27,23 +27,25 @@ const Header = () => {
 
                      <Link to='/healthTips' className="py-2 text-decoration-none ml-2">Good Health & Tips</Link>
 
-                     
-
                      <Link to='/blogs' className="py-2 text-decoration-none ml-2">Blogs</Link>
 
-     
+
                   </Nav>
-                  
+
                   <Nav>
                      {
-                        user && <FaUserCircle style={{ fontSize: '2rem' }}></FaUserCircle>
+                     user ? <img src={user.photoURL} alt="" style={{height:40}} />
+                           :
+                           <FaUserCircle style={{ fontSize: '2rem' }}></FaUserCircle>
                      }
 
-                     {
-                        user ?
-                           <Button onClick={handleLogOut} variant="secondary">LogOut</Button> :
-                           <Link to='/login'><Button variant="secondary">Login</Button></Link>
-                     }
+                     <div className="px-4">
+                        {
+                           user ?
+                              <Button onClick={handleLogOut} variant="secondary">LogOut</Button> :
+                              <Link to='/login'><Button variant="secondary">Login</Button></Link>
+                        }
+                     </div>
                   </Nav>
                </Navbar.Collapse>
             </Container>
